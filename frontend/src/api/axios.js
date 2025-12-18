fd4fb864205e2d372bdf08aa1090510d63598ee7
Vikash88Kumar <vikashonly88@gmail.com>
2025-12-18 16:17:42 +0530

    Use env-based API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,8 @@
-import axios from 'axios';
-const api=axios.create({
-    baseURL:"http://localhost:5000/api/v1",
-    withCredentials:true
-})
-export default api
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+export default api;
