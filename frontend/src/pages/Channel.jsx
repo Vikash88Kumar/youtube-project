13beb88b1b1f4f92147417ab97680cdae611633a
Vikash88Kumar <vikashonly88@gmail.com>
2025-12-20 20:45:10 +0530

    subscriber lenght

--- frontend/src/pages/Channel.jsx
+++ frontend/src/pages/Channel.jsx
@@ -461,7 +461,7 @@ const Channel = () => {
 
               <TabsContent value="subscribers" className="mt-0">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pb-8">
-                  {subscribers.length ===0 ?(  <p className="text-muted-foreground">No subscribers yet</p>
+                  {subscribers?.length ===0 ?(  <p className="text-muted-foreground">No subscribers yet</p>
                   ) : ( subscribers?.map((subscriber) => {
                   return (
                       <div
