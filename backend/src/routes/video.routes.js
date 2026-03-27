5cb3389423fd64c0aac3d35543852a7ed75146b5
Vikash88Kumar <vikashonly88@gmail.com>
2026-03-27 14:52:08 +0530

    version3.4

--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -12,7 +12,7 @@ import {upload} from "../middlewares/multer.middleware.js"
 import { addVideoTowatchHistory, getWatchHistory, removeWatchHistory } from '../controllers/watchhistory.controller.js';
 
 const router = Router();
-// router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
+router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/")
