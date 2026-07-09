71e303afe6ae0665c4018cfd3dd0912d48989264
Vikash88Kumar <vikashonly88@gmail.com>
2026-07-09 21:53:01 +0530

    updates

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -15,7 +15,7 @@ firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
+messaging.onBackgroundMessage((payload)=> {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification?.title || 'New Notification';
   const notificationOptions = {
