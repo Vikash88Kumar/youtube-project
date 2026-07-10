36c7b13ec10407b15c80dc3e07464270a961fd46
Vikash88Kumar <vikashonly88@gmail.com>
2026-07-10 11:49:43 +0530

    api instance

--- frontend/src/services/notification.api.js
+++ frontend/src/services/notification.api.js
@@ -1,9 +1,10 @@
-import axiosInstance from "../api/axios.js";
-// import api from "../api/axios.js"
+import api from "../api/axios.js";
+
+
 export const getNotifications = async (page = 1, limit = 20) => {
-  return await axiosInstance.get(`/notifications?page=${page}&limit=${limit}`);
+  return await api.get(`/notifications?page=${page}&limit=${limit}`);
 };
 
 export const markAllNotificationsRead = async () => {
-  return await axiosInstance.patch("/notifications/mark-all-read");
+  return await api.patch("/notifications/mark-all-read");
 };
